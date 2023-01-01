6732fa346accfcca193afd5c3b96441dfe6f3b56
Nghi Quyen <quyenkhanhnghi317@gmail.com>
2023-01-02 01:53:53 +0200

    fetch data using useEffect

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,24 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
+import axios from 'axios'
 import Filter from './Filter'
 import Persons from './Persons'
 import PersonForm from './Personform'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('')
 
-
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(respone =>
+        setPersons(respone.data)
+      )
+  }, [])
+  
   const addName = (event) => {
     event.preventDefault();
     
@@ -49,7 +52,6 @@ const App = () => {
       return person;
     }
   })
-  console.log(filterPerson);
 
   return (
     <div>
